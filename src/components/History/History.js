ae35dd4488cc1cebf50875318eb518dcc9d762bf
Kapilan Navaratnam <kapilan@syomortgage.com>
2020-07-09 02:28:18 -0400

    Edited copy in job description

--- src/components/History/History.js
+++ src/components/History/History.js
@@ -36,10 +36,10 @@ function History(){
                 </li>
 
         <li>Optimize loading speeds and capacity</li>
-        <li>Troubleshoot and debug issues on Intelex web properties</li>
+        <li>Troubleshoot and debug issues on website</li>
         <li>Stay current with digital web technologies for continuous improvement and optimization</li>
         <li>Monitor and analyze site performance (e.g. traffic, conversions)</li>
-<li>Participated in regularly scheduled stand-ups with the project team to understand requirements, suggest product improvements, explain code design, provide estimates, describe work 
+<li>Participated in regularly scheduled stand-ups with team to understand requirements, suggest product improvements, explain code design, provide estimates, describe work 
         progress, and identify risks to timelines.</li>
         </p>
         </Tab.Pane>
@@ -49,10 +49,10 @@ function History(){
         </div><p>
             <li>Design UI/UX, colour scheme, and wireframes using Photoshop XD and Figma.</li>
             <li>Developed custom mobile responsive WordPress website using HTML, CSS, JavaScript and PHP. </li>
+            <li>Demonstrate the ability to estimate project effort and meet deadlines</li>
             <li>Completed daily JIRA tickets to update Call to Actions, Landing Pages, Blog Content, and Newsletters. </li>
             <li>Version controlled the website using Git on the development server before deploying to production.</li>
             <li>UX/UI expert for team: helps shape the usability and experience of both external and internal applications</li>
-            <li>Creates, reviews, and consults on user flows, information architecture, interaction design, layout, and visual presentation</li>
             <li>Collaborates with stakeholders on requirements definition and business analysis</li>
 </p>
         </Tab.Pane>
@@ -61,15 +61,11 @@ function History(){
         Freelance
         </div><p>
             <li>Translates visual mock-ups into web experience</li>
-            <li>Demonstrate the ability to estimate project effort and meet deadlines</li>
             <li>Prioritize tasks in a fast-paced and agile environment with strong attention to detail</li>
             <li>Recommends platforms based on marketing / business goals</li>
             <li>Collaborates with marketing team and art directors and other site stakeholders to implement designs</li>
             <li>Makes SEO recommendations and implements optimization strategies</li>
-            <li>Designs creative, front-end digital experiences, and translates designs into working responsive 
-                front-end code, and measurable business results (e.g. lead generation, 
-                conversion to purchase, or persuasion)</li>
-
+            <li>Creates, reviews, and consults on user flows, information architecture, interaction design, layout, and visual presentation</li>
 </p>
         </Tab.Pane>
       </Tab.Content>
